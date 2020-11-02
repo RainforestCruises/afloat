860a8b3461cd4c3dfb51d7092906ba9e0d062430
Ryan <ryan@rainforestcruises.com>
2020-11-02 16:06:37 -0500

    7

--- single-rfc_cruises.php
+++ single-rfc_cruises.php
@@ -64,7 +64,7 @@ while (have_posts()) :
         <?php
         get_template_part('template-parts/content', 'product-prices', $args);
         ?>
-      </div> 
+      </div>  
    
       <!-- 5. Dates Content --> 
       <div class="product-content__page tab-content " id="tab-dates">
