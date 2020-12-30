126e5f8452cc89cadc55720155a381d168b3bfbe
Ryan <ryan@rainforestcruises.com>
2020-12-30 16:06:56 -0500

    travel guides single 1

--- single-rfc_travel_guides.php
+++ single-rfc_travel_guides.php
@@ -1,5 +1,6 @@
 <?php
 get_header();
+wp_enqueue_script('page-travel-guide', get_template_directory_uri() . '/js/page-travel-guide.js', array('jquery'), false, true);
 
 ?>
 <?php
@@ -42,9 +43,150 @@ while (have_posts()) :
       <div class="travel-guide__content drop-cap-1a">
         <?php echo the_content(); ?>
       </div>
+      <div class="travel-guide__question">
+        <div class="travel-guide__question__title">
+          Any Questions? Ask Us!
+        </div>
+
+        <span class="travel-guide__question__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nesciunt architecto, iste sit similique hic.</span>
+        <button class="travel-guide__question__button">
+          Ask us a question
+          <svg>
+            <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-arrow-right"></use>
+          </svg>
+        </button>
+      </div>
+      <div class="travel-guide__entry">
+        This entry was posted ----
+      </div>
+      <div class="travel-guide__social">
+        <svg>
+          <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-compass"></use>
+        </svg>
+        <svg>
+          <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-compass"></use>
+        </svg>
+        <svg>
+          <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-compass"></use>
+        </svg>
+        <svg>
+          <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-compass"></use>
+        </svg>
+        <svg>
+          <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-compass"></use>
+        </svg>
+      </div>
     </div>
+    <div class="travel-guide-related">
+      <div class="travel-guide-related__title">
+        You may also like
+      </div>
+      <div class="travel-guide-related__slider-area">
+        <div class="travel-guide-related__slider-area__slider" id="related-slider">
 
+          <!-- Item -->
+          <div class="travel-guide-related__slider-area__slider__item">
+            <img src="<?php echo esc_url($featured_image['url']); ?>" alt="product" class="travel-guide-related__slider-area__slider__item__image">
+            <div class="travel-guide-related__slider-area__slider__item__content">
+              <div class="travel-guide-related__slider-area__slider__item__content__title">
+                5 Things to do in the Amazon
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta ratione libero sint accusantium minus voluptate dolore? Sit repudiandae molestiae
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__cta">
+                <button class="goto-button goto-button--small">
+                  Read More
+                  <svg>
+                    <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-arrow-right"></use>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
 
+          <!-- Item -->
+          <div class="travel-guide-related__slider-area__slider__item">
+            <img src="<?php echo esc_url($featured_image['url']); ?>" alt="product" class="travel-guide-related__slider-area__slider__item__image">
+            <div class="travel-guide-related__slider-area__slider__item__content">
+              <div class="travel-guide-related__slider-area__slider__item__content__title">
+                5 Things to do in the Amazon
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta ratione libero sint accusantium minus voluptate dolore? Sit repudiandae molestiae
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__cta">
+                <button class="goto-button goto-button--small">
+                  Read More
+                  <svg>
+                    <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-arrow-right"></use>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- Item -->
+          <div class="travel-guide-related__slider-area__slider__item">
+            <img src="<?php echo esc_url($featured_image['url']); ?>" alt="product" class="travel-guide-related__slider-area__slider__item__image">
+            <div class="travel-guide-related__slider-area__slider__item__content">
+              <div class="travel-guide-related__slider-area__slider__item__content__title">
+                5 Things to do in the Amazon
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta ratione libero sint accusantium minus voluptate dolore? Sit repudiandae molestiae
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__cta">
+                <button class="goto-button goto-button--small">
+                  Read More
+                  <svg>
+                    <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-arrow-right"></use>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- Item -->
+          <div class="travel-guide-related__slider-area__slider__item">
+            <img src="<?php echo esc_url($featured_image['url']); ?>" alt="product" class="travel-guide-related__slider-area__slider__item__image">
+            <div class="travel-guide-related__slider-area__slider__item__content">
+              <div class="travel-guide-related__slider-area__slider__item__content__title">
+                5 Things to do in the Amazon
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta ratione libero sint accusantium minus voluptate dolore? Sit repudiandae molestiae
+              </div>
+              <div class="travel-guide-related__slider-area__slider__item__content__cta">
+                <button class="goto-button goto-button--small">
+                  Read More
+                  <svg>
+                    <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-arrow-right"></use>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="travel-guide-newsletter">
+      <div class="travel-guide-newsletter__content">
+        <div class="travel-guide-newsletter__content__title">
+          Stay up to date with the latest
+        </div>
+        <div class="travel-guide-newsletter__content__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, perspiciatis deserunt id dolore suscipit fuga accusantium voluptas atque consectetur!
+        </div>
+        <div class="travel-guide-newsletter__content__email">
+          <input type="text" placeholder="Enter your email" class="travel-guide-newsletter__content__email__input">
+          <button class="travel-guide-newsletter__content__email__button">Submit</button>
+        </div>
+      </div>
+      <div class="travel-guide-newsletter__image">
+        <img src="<?php echo esc_url($featured_image['url']); ?>" alt="">
+
+      </div>
+    </div>
   </div>
 
 
@@ -53,3 +195,8 @@ endwhile;
 ?>
 <!-- #site-wrapper end-->
 <?php get_footer() ?>
+
+
+<script>
+  var templateUrl = "<?php echo bloginfo('template_url') ?>";
+</script>
--- snippets.php
+++ snippets.php
@@ -1,14 +1,29 @@
-<?php
-                        $count = 0;
-                        if ($countries) {
-                            foreach ($countries as $country) {
-                                $title = get_the_title($country->ID);
-                                if ($count != 0) {
-                                    echo " / " . $title;
-                                } else {
-                                    echo $title;
-                                }
-                                $count++;
-                            }
-                        }
-                        ?>
+<div class="travel-guide-related__slider-area__slider__item">
+          <img src="<?php echo esc_url($featured_image['url']); ?>" alt="product" class="travel-guide-related__slider-area__slider__item__image">
+          <div class="travel-guide-related__slider-area__slider__item__content">
+            <div class="travel-guide-related__slider-area__slider__item__content__title">
+              5 Things to do in the Amazon
+            </div>
+            <div class="travel-guide-related__slider-area__slider__item__content__text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta ratione libero sint accusantium minus voluptate dolore? Sit repudiandae molestiae
+            </div>
+            <div class="travel-guide-related__slider-area__slider__item__content__cta">
+              <button class="goto-button goto-button--small">
+                Read More
+                <svg>
+                  <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-arrow-right"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+        </div>
+        <div class="travel-guide-related__slider-area__slider__item">
+          Item 2
+        </div>
+        <div class="travel-guide-related__slider-area__slider__item">
+          Item 3
+        </div>
+        <div class="travel-guide-related__slider-area__slider__item">
+          Item 4
+        </div>
