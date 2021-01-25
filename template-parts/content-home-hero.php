bf119f81854e22bea16e614de2c3adb3439f5666
Ryan <ryan@rainforestcruises.com>
2021-01-25 12:35:43 -0500

    home css corrected

--- template-parts/content-home-hero.php
+++ template-parts/content-home-hero.php
@@ -15,22 +15,20 @@
                         Tailor Made Adventure Travel
                     </div>
                 </div>
-                <div class="home-hero__content__cta">
-                    <button class="home-hero__content__cta__button">
-                        Explore Destinations
-                    </button>
-                </div>
+                
                 <form class="home-hero__content__search-form">
-                    <!-- Destination -->
-                    <div class="home-hero__content__search-form__form-group">
-                        <div class="home-hero__content__search-form__form-group__icon">
+
+                <div class="home-hero__content__search-form__inputs">
+                      <!-- Destination -->
+                      <div class="home-hero__content__search-form__inputs__form-group">
+                        <div class="home-hero__content__search-form__inputs__form-group__icon">
                             <svg>
                                 <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-boat-front"></use>
                             </svg>
                         </div>
-                        <div class="home-hero__content__search-form__form-group__input-group">
-                            <label for="destination" class="home-hero__content__search-form__form-group__input-group__label">Destination</label>
-                            <input class="home-hero__content__search-form__form-group__input-group__input" id="destination" tabindex="1" placeholder="Where would you like to go?">
+                        <div class="home-hero__content__search-form__inputs__form-group__input-group">
+                            <label for="destination" class="home-hero__content__search-form__inputs__form-group__input-group__label">Destination</label>
+                            <input class="home-hero__content__search-form__inputs__form-group__input-group__input" id="destination" tabindex="1" placeholder="Where would you like to go?">
 
                             </input>
                         </div>
@@ -38,18 +36,26 @@
                     </div>
 
                     <!-- Dates -->
-                    <div class="home-hero__content__search-form__form-group">
-                        <div class="home-hero__content__search-form__form-group__icon">
+                    <div class="home-hero__content__search-form__inputs__form-group">
+                        <div class="home-hero__content__search-form__inputs__form-group__icon">
                             <svg>
                                 <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-event-confirm"></use>
                             </svg>
                         </div>
-                        <div class="home-hero__content__search-form__form-group__input-group">
-                            <label for="dates" class="home-hero__content__search-form__form-group__input-group__label">Travel
+                        <div class="home-hero__content__search-form__inputs__form-group__input-group">
+                            <label for="dates" class="home-hero__content__search-form__inputs__form-group__input-group__label">Travel
                                 Date</label>
-                            <input type="text" class="home-hero__content__search-form__form-group__input-group__input" id="dates" tabindex="1" placeholder="When would you like to travel?" />
+                            <input type="text" class="home-hero__content__search-form__inputs__form-group__input-group__input" id="dates" tabindex="1" placeholder="When would you like to travel?" />
                         </div>
                     </div>
+                </div>
+              
+
+                    <div class="home-hero__content__search-form__cta">
+                    <button class="home-hero__content__search-form__cta__button">
+                        Search
+                    </button>
+                </div>
                 </form>
             </div>
         </div>
