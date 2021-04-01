a3d69370dceb6d878223cf4e93a2b457578ae299
Ryan <ryan@rainforestcruises.com>
2021-04-01 15:25:13 -0500

    breadcrumbs

--- template-parts/content-product-hero-nav.php
+++ template-parts/content-product-hero-nav.php
@@ -1,6 +1,8 @@
 <?php
 $hero_image = get_field('hero_image');
 $productTitle = get_the_title();
+$breadcrumb = get_field('breadcrumb');
+
 $showOverview = true;
 if (get_post_type() == 'rfc_tours') {
     $productTitle = get_field('tour_name');
@@ -28,35 +30,57 @@ if ($args['propertyType'] == 'Cruise') {
         <!-- Title / Navigation -->
         <div class="product-hero__top__content">
 
-            <!-- H1 Title / Subtitle -->
-            <div class="product-hero__top__content__title-group">
-                <h1 class="product-hero__top__content__title-group__title" id="template-nav-title"><?php echo $productTitle ?></h1>
-                <h2 class="product-hero__top__content__title-group__subtitle"><?php echo get_field('top_snippet') ?></h2>
-            </div>
+            <!-- Breadcrumb -->
+            <ol class="product-hero__top__content__breadcrumb">
+                <li>
+                    <a href="<?php echo home_url() ?>">Home</a>
+                </li>
+                <?php
+                if ($breadcrumb) :
+                    foreach ($breadcrumb as $b) :
+                        if ($b['link'] != null) : ?>
+                            <li>
+                                <a href=" <?php echo $b['link']  ?>"><?php echo $b['title'] ?></a>
+                            </li>
+                        <?php else : ?>
+                            <li>
+                                <?php echo $b['title'] ?>
+                            </li>
+                <?php endif;
+                    endforeach;
+                endif; ?>
 
-
-            <!-- Navigation Wrapper -->
-            <nav class="product-hero__top__content__nav" id="template-nav">
-
-                <!-- Hidden title -->
-                <div class="page-nav-title">
-                    <?php echo $productTitle ?>
+            </ol>
+            <div>
+                <!-- H1 Title / Subtitle -->
+                <div class="product-hero__top__content__title-group">
+                    <h1 class="product-hero__top__content__title-group__title" id="template-nav-title"><?php echo $productTitle ?></h1>
+                    <h2 class="product-hero__top__content__title-group__subtitle"><?php echo get_field('top_snippet') ?></h2>
                 </div>
 
-                <!-- nav list -->
-                <ul class="product-hero__top__content__nav__list">
-                    <?php if ($showOverview) : ?>
-                        <li class="product-hero__top__content__nav__list__item">
-                            <a href="#overview" class="product-hero__top__content__nav__list__item__link page-nav-template">Overview</a>
+
+                <!-- Navigation Wrapper -->
+                <nav class="product-hero__top__content__nav" id="template-nav">
+
+                    <!-- Hidden title -->
+                    <div class="page-nav-title">
+                        <?php echo $productTitle ?>
+                    </div>
+
+                    <!-- nav list -->
+                    <ul class="product-hero__top__content__nav__list">
+                        <?php if ($showOverview) : ?>
+                            <li class="product-hero__top__content__nav__list__item">
+                                <a href="#overview" class="product-hero__top__content__nav__list__item__link page-nav-template">Overview</a>
+                            </li>
+                        <?php endif; ?>
+                        <li class="product-hero__top__content__nav__list__item <?php echo ($showOverview ? '' : 'current') ?>">
+                            <a href="#itineraries" class="product-hero__top__content__nav__list__item__link page-nav-template"><?php echo (get_post_type() == 'rfc_cruises') ? ('Itineraries') : ('Itinerary'); ?></a>
                         </li>
-                    <?php endif; ?>
-                    <li class="product-hero__top__content__nav__list__item <?php echo ($showOverview ? '' : 'current') ?>">
-                        <a href="#itineraries" class="product-hero__top__content__nav__list__item__link page-nav-template"><?php echo (get_post_type() == 'rfc_cruises') ? ('Itineraries') : ('Itinerary'); ?></a>
-                    </li>
-                    <li class="product-hero__top__content__nav__list__item ">
-                        <a href="#accommodations" class="product-hero__top__content__nav__list__item__link page-nav-template">Accommodations</a>
-                    </li>
-                    <!-- <li class="product-hero__top__content__nav__list__item ">
+                        <li class="product-hero__top__content__nav__list__item ">
+                            <a href="#accommodations" class="product-hero__top__content__nav__list__item__link page-nav-template">Accommodations</a>
+                        </li>
+                        <!-- <li class="product-hero__top__content__nav__list__item ">
                         <a href="#prices" class="product-hero__top__content__nav__list__item__link page-nav-template">Prices</a>
                     </li>
                     <?php if (get_post_type() == 'rfc_cruises') { ?>
@@ -66,40 +90,40 @@ if ($args['propertyType'] == 'Cruise') {
                             </li>
                     <?php endif;
                     } ?> -->
-                </ul>
+                    </ul>
 
-                <!-- mobile menu header -->
-                <div class="page-nav">
-                    <div class="page-nav__button">
-                        <!-- for tour name Tour Name -->
-                        <?php echo get_the_title() ?>
-                        <svg>
-                            <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-chevron-right"></use>
-                        </svg>
+                    <!-- mobile menu header -->
+                    <div class="page-nav">
+                        <div class="page-nav__button">
+                            <!-- for tour name Tour Name -->
+                            <?php echo get_the_title() ?>
+                            <svg>
+                                <use xlink:href="<?php echo bloginfo('template_url') ?>/css/img/sprite.svg#icon-chevron-right"></use>
+                            </svg>
+                        </div>
+
+                        <div class="page-nav__cta">
+                            <button class="btn-cta-square btn-cta-square--small btn-cta-square--white">
+                                Inquire
+                            </button>
+                        </div>
                     </div>
 
-                    <div class="page-nav__cta">
-                        <button class="btn-cta-square btn-cta-square--small btn-cta-square--white">
-                            Inquire
-                        </button>
-                    </div>
-                </div>
-
-                <!--mobile menu expand-->
-                <nav class="page-nav__collapse ">
-                    <ul class="page-nav__collapse__list">
-                        <?php if ($showOverview) : ?>
+                    <!--mobile menu expand-->
+                    <nav class="page-nav__collapse ">
+                        <ul class="page-nav__collapse__list">
+                            <?php if ($showOverview) : ?>
+                                <li class="page-nav__collapse__list__item">
+                                    <a class="page-nav__collapse__list__item__link" href="#overview">Overview</a>
+                                </li>
+                            <?php endif; ?>
                             <li class="page-nav__collapse__list__item">
-                                <a class="page-nav__collapse__list__item__link" href="#overview">Overview</a>
+                                <a class="page-nav__collapse__list__item__link" href="#itineraries">Itineraries</a>
                             </li>
-                        <?php endif; ?>
-                        <li class="page-nav__collapse__list__item">
-                            <a class="page-nav__collapse__list__item__link" href="#itineraries">Itineraries</a>
-                        </li>
-                        <li class="page-nav__collapse__list__item">
-                            <a class="page-nav__collapse__list__item__link" href="#accommodations">Accommodations</a>
-                        </li>
-                        <!-- <li class="page-nav__collapse__list__item">
+                            <li class="page-nav__collapse__list__item">
+                                <a class="page-nav__collapse__list__item__link" href="#accommodations">Accommodations</a>
+                            </li>
+                            <!-- <li class="page-nav__collapse__list__item">
                             <a class="page-nav__collapse__list__item__link" href="#prices">Prices</a>
                         </li>
                         <?php if (get_post_type() == 'rfc_cruises') { ?>
@@ -107,20 +131,22 @@ if ($args['propertyType'] == 'Cruise') {
                                 <a class="page-nav__collapse__list__item__link" href="#dates">Dates</a>
                             </li>
                         <?php } ?> -->
-                    </ul>
+                        </ul>
+                    </nav>
                 </nav>
-            </nav>
 
-            <!-- CTA -->
-            <div class="product-hero__top__content__cta">
-                <button class="btn-circle btn-circle--small btn-white btn-circle--down" id="down-arrow-button" href="#overview">
-                    <svg class="btn-circle--arrow-main">
-                        <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-arrow-down"></use>
-                    </svg>
-                    <svg class="btn-circle--arrow-animate">
-                        <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-arrow-down"></use>
-                    </svg></button>
+                <!-- CTA -->
+                <div class="product-hero__top__content__cta">
+                    <button class="btn-circle btn-circle--small btn-white btn-circle--down" id="down-arrow-button" href="#overview">
+                        <svg class="btn-circle--arrow-main">
+                            <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-arrow-down"></use>
+                        </svg>
+                        <svg class="btn-circle--arrow-animate">
+                            <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-arrow-down"></use>
+                        </svg></button>
+                </div>
             </div>
+
 
             <!-- Expand Gallery -->
             <div class="product-hero__top__content__gallery-expand" id="gallery-expand-button">
@@ -138,7 +164,7 @@ if ($args['propertyType'] == 'Cruise') {
             if ($images) : ?>
                 <?php foreach ($images as $image) : ?>
                     <div class="product-hero__gallery__slick__item">
-                    
+
                         <a href="<?php echo esc_url($image['url']); ?>">
                             <img <?php afloat_responsive_image($image['id'], 'square-small', array('square-small')); ?> alt="">
 
@@ -176,9 +202,9 @@ if ($args['propertyType'] == 'Cruise') {
                             <?php echo (get_post_type($p) == 'rfc_tours') ? "Length" : "Itineraries"; ?>
                         </div>
                         <div class="product-hero__bottom__content__info-group__attributes__item__data">
-                            <?php if($charter_view == false) : ?>
-                            <?php echo (get_post_type($p) == 'rfc_tours') ? get_field('length') . " Days" : itineraryRange($args['cruiseData'], " - ") . " Days"; ?>
-                            <?php else : 
+                            <?php if ($charter_view == false) : ?>
+                                <?php echo (get_post_type($p) == 'rfc_tours') ? get_field('length') . " Days" : itineraryRange($args['cruiseData'], " - ") . " Days"; ?>
+                            <?php else :
                                 echo get_field('charter_min_days') . " Days +";
                             endif; ?>
                         </div>
@@ -188,7 +214,7 @@ if ($args['propertyType'] == 'Cruise') {
                             Capacity
                         </div>
                         <div class="product-hero__bottom__content__info-group__attributes__item__data">
-                        <?php echo (get_post_type($p) == 'rfc_tours') ? "" : get_field('vessel_capacity') . " Guests"; ?>
+                            <?php echo (get_post_type($p) == 'rfc_tours') ? "" : get_field('vessel_capacity') . " Guests"; ?>
 
                         </div>
                     </div>
