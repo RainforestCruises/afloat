b15344bb650a7704aadeb44a5aba2d82066bd7cb
Ryan <ryan@rainforestcruises.com>
2021-05-13 15:50:55 -0500

    template

--- snippets.php
+++ snippets.php
@@ -1,25 +1,96 @@
-<?php if ($searchType == 'region') : ?>
-  <label class="search-control" for="destination-select">
-    <span class="search-control__label-text">Destination</span>
-    <select class="search-control__select" id="destination-select" name="destination-select">
-      <option></option>
-      <option value="0">Any</option>
-      <?php foreach ($destinations as $destination) : ?>
-        <option value="<?php echo $destination->ID ?>"><?php echo get_the_title($destination) ?></option>
-      <?php endforeach; ?>
-    </select>
-  </label>
-<?php endif; ?>
+<div class="product-hero__bottom__content__info-group__attributes">
 
-<?php if ($searchType == 'destination' && $isBucketList == false) : ?>
-  <label class="search-control" for="location-select">
-    <span class="search-control__label-text">Location</span>
-    <select class="search-control__select" id="location-select" name="location-select">
-      <option></option>
-      <option value="0">Any</option>
-      <?php foreach ($locations as $location) : ?>
-        <option value="<?php echo $location->ID ?>"><?php echo get_field('navigation_title', $location) ?></option>
-      <?php endforeach; ?>
-    </select>
-  </label>
-<?php endif; ?>
+  <!-- Itineraries -->
+  <div class="product-hero__bottom__content__info-group__attributes__item ">
+    <div class="product-hero__bottom__content__info-group__attributes__item__data">
+      <div class="product-hero__bottom__content__info-group__attributes__item__data__icon">
+        <svg>
+          <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-m-time"></use>
+        </svg>
+      </div>
+      <div class="product-hero__bottom__content__info-group__attributes__item__data__text">
+        4 - 5 Days <div class="sub-attribute">
+          2 Itineraries
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <!-- Capacity -->
+  <div class="product-hero__bottom__content__info-group__attributes__item nomargin-attributes">
+
+    <div class="product-hero__bottom__content__info-group__attributes__item__data">
+
+      <div class="product-hero__bottom__content__info-group__attributes__item__data__icon">
+        <svg>
+          <use xlink:href="http://localhost/rfcwp/wp-content/themes/afloat/css/img/sprite.svg#icon-boat-front"></use>
+        </svg>
+      </div>
+      <div class="product-hero__bottom__content__info-group__attributes__item__data__text">
+        42 Guests <div class="sub-attribute">
+          20 Cabins </div>
+      </div>
+
+    </div>
+  </div>
+  <!-- Experiences -->
+  <div class="product-hero__bottom__content__info-group__attributes__item experience-attributes">
+    <div class="product-hero__bottom__content__info-group__attributes__item__data">
+      <ul>
+        <li>
+
+          <div class="experience-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32">
+              <title>distance</title>
+              <g class="nc-icon-wrapper" stroke-linecap="square" stroke-linejoin="miter" stroke-width="2" fill="#111111" stroke="#111111">
+                <circle cx="23" cy="18" r="3" fill="none" stroke="#111111" stroke-miterlimit="10"></circle>
+                <path d="M31,18c0,4.418-8,12-8,12s-8-7.582-8-12a8,8,0,0,1,16,0Z" fill="none" stroke="#111111" stroke-miterlimit="10"></path>
+                <circle cx="7" cy="7" r="2" fill="none" stroke="#111111" stroke-miterlimit="10"></circle>
+                <path d="M15,28a8,8,0,0,1-8-8" fill="none" stroke-miterlimit="10" data-color="color-2"></path>
+                <path d="M13,7c0,3.314-6,9-6,9S1,10.314,1,7A6,6,0,0,1,13,7Z" fill="none" stroke="#111111" stroke-miterlimit="10"></path>
+              </g>
+            </svg> <span class="tooltiptext">Luxury</span>
+          </div>
+
+        </li>
+        <li>
+
+          <div class="experience-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="64" height="64">
+              <title>honeymoon</title>
+              <g class="nc-icon-wrapper" stroke-linecap="square" stroke-linejoin="miter" stroke-width="4" fill="#111111" stroke="#111111">
+                <polyline points="16.227 52.227 24 60 27 57 23.872 47.616" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></polyline>
+                <polyline points="11.773 47.773 4 40 7 37 16.544 39.863" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></polyline>
+                <polyline points="38.724 35.176 54 60 60 54 50 24" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></polyline>
+                <polyline points="21.934 21.037 28.824 25.276 40.075 14.025 29.708 10.569" fill="none" stroke="#111111" stroke-miterlimit="10"></polyline>
+                <path d="M22.243,12.243a6,6,0,0,0-8.486-8.486L13,4.515l-.757-.758a6,6,0,0,0-8.486,8.486L4.515,13,13,21.485Z" fill="none" stroke-miterlimit="10" data-color="color-2"></path>
+                <path d="M57.95,15.95c2.733-2.734,4.733-9.166,2-11.9s-9.166-.733-11.9,2l-28,28L7.156,55.426a1.033,1.033,0,0,0,1.418,1.418L29.95,43.95Z" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></path>
+              </g>
+            </svg> <span class="tooltiptext">Family</span>
+          </div>
+
+        </li>
+        <li>
+
+          <div class="experience-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="64" height="64">
+              <title>honeymoon</title>
+              <g class="nc-icon-wrapper" stroke-linecap="square" stroke-linejoin="miter" stroke-width="4" fill="#111111" stroke="#111111">
+                <polyline points="16.227 52.227 24 60 27 57 23.872 47.616" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></polyline>
+                <polyline points="11.773 47.773 4 40 7 37 16.544 39.863" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></polyline>
+                <polyline points="38.724 35.176 54 60 60 54 50 24" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></polyline>
+                <polyline points="21.934 21.037 28.824 25.276 40.075 14.025 29.708 10.569" fill="none" stroke="#111111" stroke-miterlimit="10"></polyline>
+                <path d="M22.243,12.243a6,6,0,0,0-8.486-8.486L13,4.515l-.757-.758a6,6,0,0,0-8.486,8.486L4.515,13,13,21.485Z" fill="none" stroke-miterlimit="10" data-color="color-2"></path>
+                <path d="M57.95,15.95c2.733-2.734,4.733-9.166,2-11.9s-9.166-.733-11.9,2l-28,28L7.156,55.426a1.033,1.033,0,0,0,1.418,1.418L29.95,43.95Z" fill="none" stroke="#111111" stroke-miterlimit="10" data-cap="butt" stroke-linecap="butt"></path>
+              </g>
+            </svg> <span class="tooltiptext">Adventure</span>
+          </div>
+
+        </li>
+      </ul>
+    </div>
+  </div>
+
+
+
+</div>
