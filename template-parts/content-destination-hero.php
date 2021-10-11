d8c8c8be970b5a295b5dc6c40110a8d59ec76ba8
rlsystems <rlsystems@outlook.com>
2021-10-11 14:08:46 -0500

    fix3

--- template-parts/content-destination-hero.php
+++ template-parts/content-destination-hero.php
@@ -6,11 +6,10 @@
 
     $destinationType = $args['destinationType'];
 
-    $accommodationsDisplayText = 'Lodges';
+    $accommodationDisplayText = 'Lodges';
 
     if($destinationType == 'destination')
-        $accommodationsDisplayText = get_field('accommodations_label');
-
+        $accommodationDisplayText = get_field('accommodations_label');
         if($accommodationDisplayText == null) {
             $accommodationDisplayText = 'Lodges';
         }
@@ -95,12 +94,12 @@
                                 $hide_accommodations = get_field('hide_accommodations');
                                 if (!$hide_accommodations) { ?>
                                   <li class="destination-hero__content__page-nav__list__item">
-                                      <a href="#accommodation" class="destination-hero__content__page-nav__list__item__link page-nav-template"><?php echo $accommodationsDisplayText ?></a>
+                                      <a href="#accommodation" class="destination-hero__content__page-nav__list__item__link page-nav-template"><?php echo $accommodationDisplayText ?></a>
                                   </li>
                               <?php }
                             } else { ?>
                               <li class="destination-hero__content__page-nav__list__item">
-                                  <a href="#accommodation" class="destination-hero__content__page-nav__list__item__link page-nav-template"><?php echo $accommodationsDisplayText ?></a>
+                                  <a href="#accommodation" class="destination-hero__content__page-nav__list__item__link page-nav-template"><?php echo $accommodationDisplayText ?></a>
                               </li>
                           <?php } ?>
 
